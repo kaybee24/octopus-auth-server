ecf9b5a1b64ed13d8beb53a1fee41017cc1cc3c3
‘Katrin <katrin.baier77@gmail.com>
2022-11-24 11:51:39 +0100

    uninstall firebase

--- index.js
+++ index.js
@@ -7,32 +7,6 @@ import messageRouter from "./routes/Message"
 import userRouter from "./routes/User"
 import * as jwt from "./utilities/jwt"
 import chalk from "chalk"
-import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
-
-// Follow this pattern to import other Firebase services
-// import { } from 'firebase/<service>';
-
-// TODO: Replace the following with your app's Firebase project configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCgC5DZqT9zZ1dpeTOsO3jzJQXPi15FN2U",
-  authDomain: "octopus-eco.firebaseapp.com",
-  projectId: "octopus-eco",
-  storageBucket: "octopus-eco.appspot.com",
-  messagingSenderId: "810917542018",
-  appId: "1:810917542018:web:a80a278b1fbb7b3dec9c9b"
-};
-
-const api = initializeApp(firebaseConfig);
-const db = getFirestore(api);
-
-// Get a list of cities from your database
-async function getCities(db) {
-  const citiesCol = collection(db, 'cities');
-  const citySnapshot = await getDocs(citiesCol);
-  const cityList = citySnapshot.docs.map(doc => doc.data());
-  return cityList;
-}
 
 const app = express()
 const PORT = process.env.PORT || 8080
